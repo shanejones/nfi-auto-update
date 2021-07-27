1f30afcb895d71efe43d3e945b85eb09ee8ba712
flo <florianaltendofer@gmx.net>
2021-07-28 00:58:10 +0200

    Added the last rommit message to the telegam message

--- notify.py
+++ notify.py
@@ -3,7 +3,7 @@ import telegram
 telegram_http_api = "TELEGRAM_HTTP_API"
 telegram_user_id = "TELEGRAM_USER_ID"
 
-message = "NFI Repo Updated \n\n Please /reload_config to load the latest version and then run /start to get that running again"
+message = "NFI Repo Updated \n\n " + str(sys.argv[1]) + " \n\n Please /reload_config to load the latest version and then run /start to get that running again"
 
 bot_binance = telegram.Bot(token=telegram_http_api)
 bot_binance.send_message(chat_id=telegram_user_id, text=message)
--- update.sh
+++ update.sh
@@ -3,8 +3,9 @@ cd /root/NostalgiaForInfinity
 
 GITRESPONSE=`git pull`
 UPDATED='Already up to date.'
+GITMESSAGE=`git log -1 --pretty=%B`
 
 if [[ $GITRESPONSE != $UPDATED ]]; then
         cp NostalgiaForInfinityNext.py /root/freqtrade/user_data/strategies/NostalgiaForInfinityNext.py
-        python3 /root/nfi-auto-update/notify.py
+        python3 /root/nfi-auto-update/notify.py $GITMESSAGE
 fi
