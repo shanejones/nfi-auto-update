e957e8610fac9cbdf6944a84e518f2120647c5b7
flo <florianaltendofer@gmx.net>
2021-08-01 08:41:06 +0200

    fixed

--- update.sh
+++ update.sh
@@ -3,9 +3,16 @@ cd /root/NostalgiaForInfinity
 
 GITRESPONSE=`git pull`
 UPDATED='Already up to date.'
-GITMESSAGE=`git log -1 --pretty=%B`
 
 if [[ $GITRESPONSE != $UPDATED ]]; then
         cp NostalgiaForInfinityNext.py /root/freqtrade/user_data/strategies/NostalgiaForInfinityNext.py
-        python3 /root/nfi-auto-update/notify.py $GITMESSAGE
+        
+		GITMESSAGE=`git log -1 --pretty=%B`
+
+        if [[ $GITMESSAGE != "" ]]; then
+                python3 /root/nfi-auto-update/notify.py "$GITMESSAGE"
+        else    
+                python3 /root/nfi-auto-update/notify.py
+        fi
+
 fi
