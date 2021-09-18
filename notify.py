1b26d2524ee2a2112c1fb0a6470fed1bb0cf6e0f
ecoppen <51025241+ecoppen@users.noreply.github.com>
2021-09-18 13:00:15 +0100

    Update notify.py
    Changed the message to include HTML tags e.g. <bold> for the headings and <code> for the content

--- notify.py
+++ notify.py
@@ -4,11 +4,11 @@ import sys
 telegram_http_api = "TELEGRAM_HTTP_API"
 telegram_user_id = "TELEGRAM_USER_ID"
 
-freq = ("Freqtrade version:  %s" % (sys.argv[1]))
-nfi = ("NFI  version:  %s" % (sys.argv[2]))
-comment = ("Comment : %s" % (sys.argv[3]))
+freq = "<b>Freqtrade version</b>: <code>{}</code>".format(sys.argv[1])
+nfi = "<b>NFI  version</b>: <code>{}</code>".format(sys.argv[2])
+comment = "<b>Comment</b>: <code>{}</code>".format(sys.argv[3])
 
 message = freq + "\n" +  nfi + "\n" + comment + "\n" + "NFI was successfully updated."
 
 bot_binance = telegram.Bot(token=telegram_http_api)
-bot_binance.send_message(chat_id=telegram_user_id, text=message)
+bot_binance.send_message(chat_id=telegram_user_id, text=message, parse_mode='HTML')
