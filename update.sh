353be67b4d7395d58fee0abee6f74d6f87e56738
ecoppen <51025241+ecoppen@users.noreply.github.com>
2021-09-18 11:10:19 +0100

    Update update.sh
    Added the retrieval of freqtrade version and nfi version before running the restart shell script

--- update.sh
+++ update.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-cd /root/NostalgiaForInfinity
+cd /root/freqtrade
+FREQVERSION=`git describe --tags`
 
+cd /root/NostalgiaForInfinity
 GITRESPONSE=`git pull`
 UPDATED='Already up to date.'
+NFIVERSION=`git describe --tags`
+NFICOMMENT=$(git show -s --format='%s')
 
 if [[ $GITRESPONSE != $UPDATED ]]; then
-        cp NostalgiaForInfinityNext.py /root/freqtrade/user_data/strategies/NostalgiaForInfinityNext.py
-        
-		GITMESSAGE=`git log HEAD@{1}.. --pretty=%B`
-
-        if [[ $GITMESSAGE != "" ]]; then
-                python3 /root/nfi-auto-update/notify.py "$GITMESSAGE"
-        else    
-                python3 /root/nfi-auto-update/notify.py
-        fi
-
+  python3 /root/nfi-auto-update/notify.py "$FREQVERSION" "$NFIVERSION" "$NFICOMMENT"
+  sleep 5
+  /root/nfi-auto-update/restart.sh
 fi
